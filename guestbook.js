fc4c748c35a611895b25c562986815693e6a3b49
Tim Mattick <tmattick@smail.uni-koeln.de>
2021-07-05 11:26:01 +0200

    f strings

--- guestbook.js
+++ guestbook.js
@@ -1,5 +1,12 @@
 function make_comment() {
     var name = document.getElementById("name").value;
+    var comment = document.getElementById("comment").value;
     console.log('Funktion "make_comment" ausgeführt');
     console.log(name);
+    console.log(comment);
+
+    output = "${name} schrieb: ${comment}.";
+    console.log(output)
+
+    document.getElementById("comment_out").innerHTML += output;
 }
