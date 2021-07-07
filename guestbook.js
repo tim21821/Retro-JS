6c2aea8cd0a8acd2a057b1cf34f24a359d98d33d
Tim Mattick <tmattick@smail.uni-koeln.de>
2021-07-07 11:38:56 +0200

    answer block appears

--- guestbook.js
+++ guestbook.js
@@ -1,12 +1,28 @@
 function make_comment() {
     var name = document.getElementById("name").value;
     var comment = document.getElementById("comment").value;
-    console.log('Funktion "make_comment" ausgeführt');
-    console.log(name);
-    console.log(comment);
 
-    output = "${name} schrieb: ${comment}.";
-    console.log(output)
+    output = construct_output(comment, name);
+    console.log(output);
 
-    document.getElementById("comment_out").innerHTML += output;
+
+    document.getElementById("comment_block").innerHTML += output;
+}
+
+function make_answer(element) {
+    console.log(element);
+}
+
+function construct_output(comment, name) {
+    const DATE = new Date();
+    const ANSWER_FIELD = '<div class="answer_field" id="answer_field' + DATE.getTime() + '">' + '<label for "name' + DATE.getTime() + '">Name: </label><input type="text" id="name' + DATE.getTime() + '"><br><label for "answer' + DATE.getTime() + '">Deine Antwort:</label><br><textarea name="answer' + DATE.getTime() + '" id="answer' + DATE.getTime() + '" cols="70" rows="5" placeholder = "Hier kommt die Antwort hin ..." required></textarea><br><button onclick = "make_answer(this)" class="send_answer_button" id="answer' + DATE.getTime() + '">Absenden</button></div>';
+    var comment_output = name + " schrieb am " + DATE.getDate() + "." + DATE.getMonth() + "." + DATE.getFullYear() + " (" + DATE.getHours() + ":" + DATE.getMinutes() + " Uhr): " + comment;
+    var output = '<div id="' + name + DATE.getTime() + '">' + comment_output + "<br>" + '<button class="answer_button" onclick="display_answer_block(this)">Antworten</button>' + ANSWER_FIELD + '</div>';
+    return output;
+}
+
+function display_answer_block(element) {
+    const ID = element.parentNode.id;
+    console.log(ID);
+    element.parentNode.getElementsByClassName("answer_field")[0].style.display = "block";
 }
