4244c924222cbc610a8d2f0938c7570b117d60f8
Tim Mattick <tim.mattick@gmx.de>
2021-08-12 10:29:14 +0200

    ported tetris from python

--- games/tetris.js
+++ games/tetris.js
@@ -1,6 +1,17 @@
 const X_SIZE = 10;
 const Y_SIZE = 20
 const BLOCK_SIZE = 40;
+const SHAPES = ["O", "I", "T", "Z", "S", "L", "J"];
+const FPS = 1;
+
+var grid = new Array(Y_SIZE + 5).fill(Array(X_SIZE).fill(null));
+console.log(grid);
+var queue = [];
+var score = 0;
+var counter = 0;
+var moving = false;
+
+let current;
 
 /**
  * Wird vor Spielstart aufgerufen
@@ -8,6 +19,7 @@ const BLOCK_SIZE = 40;
  */
 function setup() {
     createCanvas(X_SIZE * BLOCK_SIZE, Y_SIZE * BLOCK_SIZE);
+    frameRate(FPS);
 }
 
 /**
@@ -19,6 +31,7 @@ function draw() {
      * Zeichnet das hintenliegende Gitter
      */
     function draw_grid() {
+        fill("white")
         for (var i = 0; i < X_SIZE; i++) {
             for (var j = 0; j < Y_SIZE; j++) {
                 rect(i * BLOCK_SIZE, j * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
@@ -28,6 +41,61 @@ function draw() {
 
     background(255);
     draw_grid();
+
+    counter++;
+    if (queue.length == 0) {
+        queue = shuffle(SHAPES);
+    }
+
+    if (moving) {
+        if (counter % 5 == 0) {
+            grid = current.destroy(grid);
+            current.move_down();
+            grid = current.make(grid);
+        }
+    } else {
+        counter = 0;
+        for (var i = 0; i < grid.length; i++) {
+            if (check_tetris(grid, i)) {
+                counter++;
+                grid = delete_line(grid, i);
+            }
+        }
+        switch (counter) {
+            case 1:
+                score += 40;
+            case 2:
+                score += 100;
+            case 3:
+                score += 300;
+            case 4:
+                score += 1200;
+        }
+        for (var j = 0; j < grid.length; j++) {
+            for (var i = 0; i < grid[j].length; i++) {
+                if (grid[j][i] != null) {
+                    grid[j][i].current = false;
+                }
+            }
+        }
+
+        current = new Shape(4, 1, queue.pop(0));
+        if (current.collide(grid, 0, 0)) {
+            console.log("Game Over!")
+            console.log(current)
+        }
+        grid = current.make(grid);
+        moving = true;
+    }
+
+    for (var j = 0; j < grid.length; j++) {
+        for (var i = 0; i < grid[j].length; i++) {
+            if (grid[j][i] != null) {
+                grid[j][i].draw();
+            }
+        }
+    }
+    console.log(grid);
 }
 
 /**
@@ -98,38 +166,38 @@ function Block(x, y, color) {
 function Shape(x, y, shape) {
     this.COLORS = { "O": "yellow", "I": "cyan", "T": "violet", "Z": "red", "S": "green", "L": "orange", "J": "blue" };
     this.OFFSETS = {
-        "O": [[(0, 0), (1, 0), (0, 1), (1, 1)]],
-        "I": [[(0, 0), (0, 1), (0, 2), (0, 3)],
-        [(-1, 1), (0, 1), (1, 1), (2, 1)],
-        [(1, 0), (1, 1), (1, 2), (1, 3)],
-        [(-1, 2), (0, 2), (1, 2), (2, 2)]],
-        "T": [[(0, 0), (-1, 1), (0, 1), (1, 1)],
-        [(0, 0), (0, 1), (1, 1), (0, 2)],
-        [(0, 2), (-1, 1), (0, 1), (1, 1)],
-        [(0, 0), (0, 1), (-1, 1), (0, 2)]],
-        "Z": [[(0, 0), (1, 0), (1, 1), (2, 1)],
-        [(2, 0), (1, 1), (2, 1), (1, 2)],
-        [(0, 1), (1, 1), (1, 2), (2, 2)],
-        [(1, 0), (1, 1), (0, 1), (0, 2)]],
-        "S": [[(1, 0), (2, 0), (1, 1), (0, 1)],
-        [(1, 0), (1, 1), (2, 1), (2, 2)],
-        [(1, 1), (2, 1), (1, 2), (0, 2)],
-        [(0, 0), (0, 1), (1, 1), (1, 2)]],
-        "L": [[(2, 0), (2, 1), (1, 1), (0, 1)],
-        [(1, 0), (1, 1), (1, 2), (2, 2)],
-        [(0, 1), (1, 1), (2, 1), (0, 2)],
-        [(0, 0), (1, 0), (1, 1), (1, 2)]],
-        "J": [[(0, 0), (0, 1), (1, 1), (2, 1)],
-        [(1, 0), (2, 0), (1, 1), (1, 2)],
-        [(0, 1), (1, 1), (2, 1), (2, 2)],
-        [(1, 0), (1, 1), (1, 2), (0, 2)]]
+        "O": [[[0, 0], [1, 0], [0, 1], [1, 1]]],
+        "I": [[[0, 0], [0, 1], [0, 2], [0, 3]],
+        [[-1, 1], [0, 1], [1, 1], [2, 1]],
+        [[1, 0], [1, 1], [1, 2], [1, 3]],
+        [[-1, 2], [0, 2], [1, 2], [2, 2]]],
+        "T": [[[0, 0], [-1, 1], [0, 1], [1, 1]],
+        [[0, 0], [0, 1], [1, 1], [0, 2]],
+        [[0, 2], [-1, 1], [0, 1], [1, 1]],
+        [[0, 0], [0, 1], [-1, 1], [0, 2]]],
+        "Z": [[[0, 0], [1, 0], [1, 1], [2, 1]],
+        [[2, 0], [1, 1], [2, 1], [1, 2]],
+        [[0, 1], [1, 1], [1, 2], [2, 2]],
+        [[1, 0], [1, 1], [0, 1], [0, 2]]],
+        "S": [[[1, 0], [2, 0], [1, 1], [0, 1]],
+        [[1, 0], [1, 1], [2, 1], [2, 2]],
+        [[1, 1], [2, 1], [1, 2], [0, 2]],
+        [[0, 0], [0, 1], [1, 1], [1, 2]]],
+        "L": [[[2, 0], [2, 1], [1, 1], [0, 1]],
+        [[1, 0], [1, 1], [1, 2], [2, 2]],
+        [[0, 1], [1, 1], [2, 1], [0, 2]],
+        [[0, 0], [1, 0], [1, 1], [1, 2]]],
+        "J": [[[0, 0], [0, 1], [1, 1], [2, 1]],
+        [[1, 0], [2, 0], [1, 1], [1, 2]],
+        [[0, 1], [1, 1], [2, 1], [2, 2]],
+        [[1, 0], [1, 1], [1, 2], [0, 2]]]
     };
 
     this.x = x;
     this.y = y;
     this.rotation = 0;
     this.type = shape;
-    this.color = this.COLORS[self.type];
+    this.color = this.COLORS[this.type];
 
     /**
      * Bewegt das Tetromino nach unten
@@ -221,7 +289,7 @@ function Shape(x, y, shape) {
         for (var i = 0; i < 4; i++) {
             x = this.x - 1 + this.OFFSETS[this.type][this.rotation][i][0];
             y = this.y - 1 + this.OFFSETS[this.type][this.rotation][i][1];
-            grid[y][x] = Block(x, y, this.color);
+            grid[y][x] = new Block(x, y, this.color);
         }
         return grid;
     }
